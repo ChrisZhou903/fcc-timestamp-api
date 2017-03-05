024c562c91ffd6ab98b02f9ace0b96fc85fce820
Chris Zhou <chris.zhou@activenetwork.com>
2017-03-05 10:03:58 +0000

    set up a timeStamp api

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 var express = require('express')
+var path = require('path')
+var moment = require('moment')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.use('/', express.static(path.join(__dirname, '')));
+
+app.get('/:time', function (req, res) {
+  var time = Number(req.params.time) ? moment.unix(req.params.time) : moment(req.params.time);
+  var unixTime = time.isValid() ? +time.format('X') : null;
+  var natural = time.isValid() ? time.format('MMMM D, YYYY') : null;
+  
+  var obj = {
+    unix: unixTime,
+    natural: natural
+  };
+  
+  try {
+    res.json(obj);
+  } catch (err) {
+    res.send(500);
+  }
+  
 })
 
 app.listen(8080, function () {
